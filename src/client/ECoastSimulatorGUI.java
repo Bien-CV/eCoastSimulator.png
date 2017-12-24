323b4db9c47ab677669d2d3a5f4ab61fbc07ea9b
Baptiste <Baptiste.cv@gmail.com>
2017-12-24 14:33:10 +0100

    elelelel

--- src/client/ECoastSimulatorGUI.java
+++ src/client/ECoastSimulatorGUI.java
@@ -19,12 +19,15 @@ import java.awt.FlowLayout;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.GridLayout;
+import javax.swing.JTextArea;
+import javax.swing.JList;
+import javax.swing.SwingConstants;
 
 @SuppressWarnings("unused")
 public class ECoastSimulatorGUI {
 
 	private JFrame frame;
-	private JTextField txtJeSuisUn;
+	private JTextField saisieChat;
 
 	/**
 	 * Launch the application.
@@ -54,7 +57,7 @@ public class ECoastSimulatorGUI {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 630, 456);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{428, 0};
@@ -104,24 +107,74 @@ public class ECoastSimulatorGUI {
 		panel_1.add(panel_3, BorderLayout.CENTER);
 		panel_3.setLayout(new BorderLayout(0, 0));
 		
-		JPanel panel_4 = new JPanel();
-		panel_3.add(panel_4, BorderLayout.SOUTH);
-		panel_4.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		JPanel panelGlobalChat = new JPanel();
+		panel_3.add(panelGlobalChat, BorderLayout.SOUTH);
+		panelGlobalChat.setLayout(new BorderLayout(0, 0));
 		
-		txtJeSuisUn = new JTextField();
-		txtJeSuisUn.setText("Je suis un chat !");
-		panel_4.add(txtJeSuisUn);
-		txtJeSuisUn.setColumns(10);
+		JTextArea affichageChat = new JTextArea();
+		affichageChat.setText("Mario: Je t'aime Baptiste\r\nJoachim: Je t'aime Baptiste omg\r\nBaptiste: Les gars stop c'est embarrassant *blush*");
+		panelGlobalChat.add(affichageChat, BorderLayout.CENTER);
 		
-		JPanel panel_5 = new JPanel();
-		panel_3.add(panel_5, BorderLayout.CENTER);
-		panel_5.setLayout(new BoxLayout(panel_5, BoxLayout.X_AXIS));
+		saisieChat = new JTextField();
+		panelGlobalChat.add(saisieChat, BorderLayout.SOUTH);
+		saisieChat.setColumns(10);
 		
-		JPanel panel_6 = new JPanel();
-		panel_5.add(panel_6);
+		JPanel panelCentreEcran = new JPanel();
+		panel_3.add(panelCentreEcran, BorderLayout.CENTER);
+		panelCentreEcran.setLayout(new BoxLayout(panelCentreEcran, BoxLayout.X_AXIS));
 		
-		JPanel panel_7 = new JPanel();
-		panel_5.add(panel_7);
+		JPanel panelGlobalListeSalle = new JPanel();
+		panelCentreEcran.add(panelGlobalListeSalle);
+		panelGlobalListeSalle.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panelBoutonsPourListeSalles = new JPanel();
+		panelGlobalListeSalle.add(panelBoutonsPourListeSalles, BorderLayout.EAST);
+		GridBagLayout gbl_panelBoutonsPourListeSalles = new GridBagLayout();
+		gbl_panelBoutonsPourListeSalles.columnWidths = new int[]{115, 115, 0};
+		gbl_panelBoutonsPourListeSalles.rowHeights = new int[]{29, 0, 0};
+		gbl_panelBoutonsPourListeSalles.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_panelBoutonsPourListeSalles.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		panelBoutonsPourListeSalles.setLayout(gbl_panelBoutonsPourListeSalles);
+		
+		JButton btnNewButton = new JButton("New button");
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton.gridx = 0;
+		gbc_btnNewButton.gridy = 0;
+		panelBoutonsPourListeSalles.add(btnNewButton, gbc_btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
+		gbc_btnNewButton_1.insets = new Insets(0, 0, 0, 5);
+		gbc_btnNewButton_1.gridx = 0;
+		gbc_btnNewButton_1.gridy = 1;
+		panelBoutonsPourListeSalles.add(btnNewButton_1, gbc_btnNewButton_1);
+		
+		JPanel panelListeSalle = new JPanel();
+		panelGlobalListeSalle.add(panelListeSalle, BorderLayout.CENTER);
+		panelListeSalle.setLayout(new BorderLayout(0, 0));
+		
+		JList listeDesSalles = new JList();
+		panelListeSalle.add(listeDesSalles);
+		
+		JPanel panelGlobalObjet = new JPanel();
+		panelCentreEcran.add(panelGlobalObjet);
+		panelGlobalObjet.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panelDescriptionObjet = new JPanel();
+		panelGlobalObjet.add(panelDescriptionObjet, BorderLayout.CENTER);
+		panelDescriptionObjet.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNomDeLobjet = new JLabel("Nom de l'objet");
+		lblNomDeLobjet.setHorizontalAlignment(SwingConstants.CENTER);
+		panelDescriptionObjet.add(lblNomDeLobjet, BorderLayout.NORTH);
+		
+		JLabel lblImagedelobjet = new JLabel("imageDeLobjet");
+		lblImagedelobjet.setHorizontalAlignment(SwingConstants.CENTER);
+		panelDescriptionObjet.add(lblImagedelobjet, BorderLayout.CENTER);
+		
+		JPanel panelBoutonsObjet = new JPanel();
+		panelGlobalObjet.add(panelBoutonsObjet, BorderLayout.SOUTH);
 	}
 
 }
