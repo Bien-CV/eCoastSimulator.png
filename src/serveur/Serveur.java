3081f9a54ce353330f6f80d177629b075c43956f
lapindead <lapindead@gmail.com>
2018-01-02 17:48:06 +0100

    clean serveur

--- src/serveur/Serveur.java
+++ src/serveur/Serveur.java
@@ -2,13 +2,9 @@
 package serveur;
 
 
-import java.net.InetAddress;
 import java.net.MalformedURLException;
-import java.net.UnknownHostException;
 import java.rmi.AccessException;
-import java.rmi.AlreadyBoundException;
 import java.rmi.Naming;
-import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -25,51 +21,14 @@ public class Serveur {
 	
 	public static void main(String[] argv) {
 		//System.setProperty("java.rmi.server.hostname", ipServeur);
-		initHdv();
-		initRegistry();
-		bindServeur();
-	}
-
-	private static void bindServeur() {
+		//initHdv();
 		try {
-			
-			DebugTools.d("Adresse serveur="+adresseServeur);
-			Naming.rebind(adresseServeur, hdv);
-		} catch (AccessException e) {
-			e.printStackTrace();
-		} catch (RemoteException e) {
-			e.printStackTrace();
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
-		
-	}
-
-	private static void initRegistry() {
-		//XXX: Get un registry fait bugger !
-		//getRegistry();
-		createRegistryIfNull();
-		
-	}
-
-
-	private static void getRegistry() {
-		try {
-			
-			r = LocateRegistry.getRegistry(PORT_SERVEUR);
-			
-			if (r!=null) {
-				DebugTools.d("Registry existant trouvé :");
-				DebugTools.d(r.toString());
-			}else {
-				DebugTools.d("Pas de registry pré-existant.");
-			}
+			hdv=new HotelDesVentes();
 		} catch (RemoteException e1) {
+			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-	}
-
-	private static void createRegistryIfNull() {
+		//initRegistry();
 		try {	
 			if (r==null) {
 				r=LocateRegistry.createRegistry(PORT_SERVEUR);
@@ -81,18 +40,17 @@ public class Serveur {
 		} catch(RemoteException  e){
 			e.printStackTrace();
 		}
-		
-	}
-
-	private static void initHdv() {
+		//bindServeur();
 		try {
-			hdv=new HotelDesVentes();
-		} catch (RemoteException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			DebugTools.d("Adresse serveur="+adresseServeur);
+			Naming.rebind(adresseServeur, hdv);
+		} catch (AccessException e) {
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
 		}
-		
 	}
-	
 }
 	
